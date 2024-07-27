aab17ab1ebdd43c155b21eb57a3347a5881d7a8e
meel516 <md.saleem516@gmail.com>
2024-07-27 05:36:31 +0530

    commit

--- src/actions.tsx/deleteAction.ts
+++ src/actions.tsx/deleteAction.ts
@@ -4,7 +4,7 @@ import {deleteEmployee} from "../_utils/_api"
 
 export const deleteAction = async(id:any)=>{
     'use server'
-
+ 
 try{
     // const formObject = Object.fromEntries(formData.entries());
 let res=await deleteEmployee(id)
